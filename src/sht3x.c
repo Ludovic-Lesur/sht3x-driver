9fc176067c07b10d3a51bb6bcf0bd5162b1f56cf
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2025-10-20 20:42:46 +0200

    Optimize conversions without float variables.

--- src/sht3x.c
+++ src/sht3x.c
@@ -17,17 +17,17 @@
 
 /*** SHT3x local macros ***/
 
-#define SHT3X_FULL_SCALE                    65535.0
+#define SHT3X_FULL_SCALE                    0xFFFF
 
 #define SHT3X_MEASURE_COMMAND_BUFFER_SIZE   2
 #define SHT3X_MEASURE_DATA_BUFFER_SIZE      6
 
-#define SHT3X_TEMPERATURE_SLOPE             175.0
-#define SHT3X_TEMPERATURE_OFFSET            (-45.0)
-#define SHT3X_TEMPERATURE_FACTOR            10.0
+#define SHT3X_CONVERSION_DELAY_MS           30
 
-#define SHT3X_HUMIDITY_SLOPE                100.0
-#define SHT3X_HUMIDITY_FACTOR               1.0
+#define SHT3X_TEMPERATURE_SLOPE             1750
+#define SHT3X_TEMPERATURE_OFFSET            (-450)
+
+#define SHT3X_HUMIDITY_SLOPE                100
 
 /*** SHT3x functions ***/
 
@@ -55,8 +55,7 @@ SHT3X_status_t SHT3X_get_temperature_humidity(uint8_t i2c_address, int32_t* temp
     SHT3X_status_t status = SHT3X_SUCCESS;
     uint8_t measure_command[SHT3X_MEASURE_COMMAND_BUFFER_SIZE] = { 0x24, 0x00 };
     uint8_t measure_buf[SHT3X_MEASURE_DATA_BUFFER_SIZE] = { 0x00 };
-    uint16_t data_16bits = 0;
-    float tmp_float = 0.0;
+    int32_t data_16bits = 0;
     // Check parameters.
     if ((temperature_tenth_degrees == NULL) || (humidity_percent == NULL)) {
         status = SHT3X_ERROR_NULL_PARAMETER;
@@ -66,19 +65,17 @@ SHT3X_status_t SHT3X_get_temperature_humidity(uint8_t i2c_address, int32_t* temp
     status = SHT3X_HW_i2c_write(i2c_address, measure_command, SHT3X_MEASURE_COMMAND_BUFFER_SIZE, 1);
     if (status != SHT3X_SUCCESS) goto errors;
     // Wait for conversion to complete (at least 15ms).
-    status = SHT3X_HW_delay_milliseconds(30);
+    status = SHT3X_HW_delay_milliseconds(SHT3X_CONVERSION_DELAY_MS);
     if (status != SHT3X_SUCCESS) goto errors;
     // Read data.
     status = SHT3X_HW_i2c_read(i2c_address, measure_buf, SHT3X_MEASURE_DATA_BUFFER_SIZE);
     if (status != SHT3X_SUCCESS) goto errors;
     // Compute temperature.
-    data_16bits = (uint16_t) ((measure_buf[0] << 8) + measure_buf[1]);
-    tmp_float = (((((float) SHT3X_TEMPERATURE_SLOPE) * ((float) data_16bits)) / ((float) SHT3X_FULL_SCALE)) + ((float) SHT3X_TEMPERATURE_OFFSET));
-    (*temperature_tenth_degrees) = (int32_t) (tmp_float * SHT3X_TEMPERATURE_FACTOR);
+    data_16bits = (int32_t) ((measure_buf[0] << 8) + measure_buf[1]);
+    (*temperature_tenth_degrees) = (((SHT3X_TEMPERATURE_SLOPE * data_16bits) / (SHT3X_FULL_SCALE)) + SHT3X_TEMPERATURE_OFFSET);
     // Compute humidity.
-    data_16bits = (uint16_t) ((measure_buf[3] << 8) + measure_buf[4]);
-    tmp_float = (((float) SHT3X_HUMIDITY_SLOPE) * ((float) data_16bits)) / ((float) SHT3X_FULL_SCALE);
-    (*humidity_percent) = (int32_t) (tmp_float * SHT3X_HUMIDITY_FACTOR);
+    data_16bits = (int32_t) ((measure_buf[3] << 8) + measure_buf[4]);
+    (*humidity_percent) = (((SHT3X_HUMIDITY_SLOPE * data_16bits)) / (SHT3X_FULL_SCALE));
 errors:
     return status;
 }
